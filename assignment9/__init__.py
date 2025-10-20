9048b56afd7a4eca29e8fd4010915fe0ee823363
Upendra Roul <roul@adobe.com>
2025-10-20 12:27:31 +0530

    module import changes

--- assignment9/__init__.py
+++ assignment9/__init__.py
@@ -4,9 +4,9 @@ Assignment 9: ResNet50 ImageNet Training
 This package contains ResNet50 implementation for ImageNet-1K classification.
 """
 
-from .model import ResNet50ImageNet, resnet50
-from .data import get_imagenet_data_loaders, ImageNetDataset
-from .config import TrainingConfig, get_default_config
+from ERAV4.assignment9.model import ResNet50ImageNet, resnet50
+from ERAV4.assignment9.data import get_imagenet_data_loaders, ImageNetDataset
+from ERAV4.assignment9.config import TrainingConfig, get_default_config
 
 __version__ = '1.0.0'
 __all__ = [
